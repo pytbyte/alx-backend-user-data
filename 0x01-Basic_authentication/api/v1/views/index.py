18055962b050da1e64e7f93bcd539de8386a6036
pytbyte <heretolearn1@gmail.com>
2024-02-13 23:30:12 +0300

    Flask app setup

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,7 +35,7 @@ def not_authorized() -> None:
     abort(401)
 
 
-@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> None:
     """GET /api/v1/forbidden
     Return:
