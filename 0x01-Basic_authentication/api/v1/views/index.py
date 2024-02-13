ac3d9f991ef24c42511400e0691c6dd176e91898
pytbyte <heretolearn1@gmail.com>
2024-02-13 17:15:50 +0300

    Flask app setup

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def not_authorized() -> None:
+    """ GET /api/v1/unauthorized
+    Return:
+      - Abort connection
+    """
+    abort(401)
